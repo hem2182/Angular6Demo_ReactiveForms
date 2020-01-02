8a7cd142af27f86a859788c096c531e3e97a2b37
hem2182 <hemant_2182_fresco@yahoo.com>
2020-01-02 22:45:17 +0530

    Angular reactive form array example and cross field validation for confirm email custom validation

--- src/app/Shared/custom.validators.ts
+++ src/app/Shared/custom.validators.ts
@@ -2,16 +2,17 @@ import { AbstractControl } from '@angular/forms';
 
 export class CustomValidators {
 
-    static emailDomain(domainName: string) {
-        return (control: AbstractControl): {[key: string]: any} | null => {
-          const email: string = control.value;
-          const domain: string = email.substring(email.lastIndexOf('@') + 1);
-          if (email === '' || domain.toLowerCase() === domainName.toLowerCase()) {
-            return null;
-          } else {
-            // true is set as the value to indicate we have emailDomain error.
-            return { 'emailDomain': true };
-          }
-        };
+  static emailDomain(domainName: string) {
+    return (control: AbstractControl): {[key: string]: any} | null => {
+      const email: string = control.value;
+      const domain: string = email.substring(email.lastIndexOf('@') + 1);
+      if (email === '' || domain.toLowerCase() === domainName.toLowerCase()) {
+        return null;
+      } else {
+        // true is set as the value to indicate we have emailDomain error.
+        // tslint:disable-next-line: object-literal-key-quotes
+        return { 'emailDomain': true };
       }
+    };
+  }
 }
--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, AbstractControl} from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl, FormArray, FormControl} from '@angular/forms';
 import { CustomValidators } from '../Shared/custom.validators';
 
 @Component({
@@ -11,34 +11,69 @@ export class CreateEmployeeComponent implements OnInit {
 
   employeeForm: FormGroup;
   validationMessages = {
-    'fullName':{
+    // tslint:disable-next-line: object-literal-key-quotes
+    'fullName': {
+      // tslint:disable-next-line: object-literal-key-quotes
       'required': 'Full Name is required.',
+      // tslint:disable-next-line: object-literal-key-quotes
       'minlength': 'Full name must be greater than 2 characters and less than 10 characters.',
+      // tslint:disable-next-line: object-literal-key-quotes
       'maxlength': 'Full name must be greater than 2 characters and less than 10 characters.',
     },
+    // tslint:disable-next-line: object-literal-key-quotes
     'email': {
+      // tslint:disable-next-line: object-literal-key-quotes
       'required': 'Email is required.',
+      // tslint:disable-next-line: object-literal-key-quotes
       'emailDomain': 'Email domain should be saxobank.com',
     },
+    // tslint:disable-next-line: object-literal-key-quotes
+    'confirmEmail': {
+      // tslint:disable-next-line: object-literal-key-quotes
+      'required': 'Confirm Email is required.',
+    },
+    // tslint:disable-next-line: object-literal-key-quotes
+    'emailGroup': {
+      // tslint:disable-next-line: object-literal-key-quotes
+      'emailMismatch': 'Email and Confirm Email do not match.'
+    },
+    // tslint:disable-next-line: object-literal-key-quotes
     'phone': {
+      // tslint:disable-next-line: object-literal-key-quotes
       'required': 'Phone is required.',
     },
+    // tslint:disable-next-line: object-literal-key-quotes
     'skillName': {
+      // tslint:disable-next-line: object-literal-key-quotes
       'required': 'Skill Name is required',
     },
+    // tslint:disable-next-line: object-literal-key-quotes
     'experienceInYears': {
+      // tslint:disable-next-line: object-literal-key-quotes
       'required': 'Experience is required',
     },
+    // tslint:disable-next-line: object-literal-key-quotes
     'proficiency': {
+      // tslint:disable-next-line: object-literal-key-quotes
       'required': 'Proficiency is required',
     },
   };
   formErrors = {
+    // tslint:disable-next-line: object-literal-key-quotes
     'fullName': '',
+    // tslint:disable-next-line: object-literal-key-quotes
     'email': '',
+    // tslint:disable-next-line: object-literal-key-quotes
+    'confirmEmail': '',
+    // tslint:disable-next-line: object-literal-key-quotes
+    'emailGroup': '',
+    // tslint:disable-next-line: object-literal-key-quotes
     'phone': '',
+    // tslint:disable-next-line: object-literal-key-quotes
     'skillName': '',
+    // tslint:disable-next-line: object-literal-key-quotes
     'experienceInYears': '',
+    // tslint:disable-next-line: object-literal-key-quotes
     'proficiency': ''
   };
 
@@ -48,13 +83,14 @@ export class CreateEmployeeComponent implements OnInit {
     this.employeeForm = this.formBuilder.group({
       fullName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(10)]],
       contactPreference: ['email'],
-      email: ['', [Validators.required, CustomValidators.emailDomain('saxobank.com')]],
       phone: [''],
-      skills: this.formBuilder.group({
-        skillName: ['',[Validators.required]],
-        experienceInYears: ['',[Validators.required]],
-        proficiency: ['',[Validators.required]]
-      })
+      skills: this.formBuilder.array([
+        this.addSkillFormGroup()
+      ]),
+      emailGroup: this.formBuilder.group({
+        email: ['', [Validators.required, CustomValidators.emailDomain('saxobank.com')]],
+        confirmEmail: ['', [Validators.required]],
+      }, {validator: matchEmail}),
     });
 
     this.employeeForm.valueChanges.subscribe((data: any) => {
@@ -68,6 +104,13 @@ export class CreateEmployeeComponent implements OnInit {
 
   }
 
+  addSkillFormGroup(): FormGroup {
+    return this.formBuilder.group({
+      skillName: ['', [Validators.required]],
+      experienceInYears: ['', [Validators.required]],
+      proficiency: ['', [Validators.required]]
+    });
+  }
   onContactPreferenceChange(selectedValue: string): void {
     const phoneControl = this.employeeForm.get('phone');
     if (selectedValue === 'phone') {
@@ -85,19 +128,27 @@ export class CreateEmployeeComponent implements OnInit {
   logValidationErrors(group: FormGroup = this.employeeForm): void {
     Object.keys(group.controls).forEach((key: string) => {
       const abstractControl = group.get(key);
+      this.formErrors[key] = '';
+      // console.log('Key = ' + key + ' Value = ' + abstractControl.value);
+      // Check if there are validation errors.
+      if (abstractControl && !abstractControl.valid && (abstractControl.touched || abstractControl.dirty )) {
+        // this means the form control has failed validation.
+        const messages = this.validationMessages[key];
+        for (const errorKey in abstractControl.errors) {
+          if (errorKey) {
+            this.formErrors[key] += messages[errorKey] + ',';
+          }
+        }
+      }
+
       if (abstractControl instanceof FormGroup) {
         this.logValidationErrors(abstractControl);
-      } else {
-        this.formErrors[key] = '';
-        // console.log('Key = ' + key + ' Value = ' + abstractControl.value);
-        // Check if there are validation errors.
-        if (abstractControl && !abstractControl.valid && (abstractControl.touched || abstractControl.dirty )) {
-          // this means the form control has failed validation.
-          const messages = this.validationMessages[key];
-          for (const errorKey in abstractControl.errors) {
-            if (errorKey) {
-              this.formErrors[key] += messages[errorKey] + ',';
-            }
+      }
+
+      if (abstractControl instanceof FormArray) {
+        for (const control of abstractControl.controls) {
+          if (control instanceof FormGroup) {
+            this.logValidationErrors(control);
           }
         }
       }
@@ -105,7 +156,37 @@ export class CreateEmployeeComponent implements OnInit {
   }
 
   onLoadDataClick(): void {
-    this.logValidationErrors(this.employeeForm);
-    console.log(this.formErrors);
+    // this.logValidationErrors(this.employeeForm);
+    // console.log(this.formErrors);
+    // const formArray = new FormArray([
+    //   new FormControl('John', Validators.required),
+    //   new FormControl(''),
+    //   new FormGroup({
+    //     country: new FormControl('', Validators.required),
+    //     state: new FormControl('')
+    //   }),
+    //   new FormArray([]),
+    // ]);
+    // to programmatically find the number of elements in the form array, we use the length property
+    // console.log(formArray.length);
+
+    // to iterate over the elements of the form array, use the for loop.
+    // for (const control of formArray.controls) {
+    //   if (control instanceof FormControl) { console.log('This is a form control.'); }
+    //   if (control instanceof FormGroup) { console.log('This is a form group.'); }
+    //   if (control instanceof FormArray) { console.log('This is a form array.'); }
+    // }
   }
+}
+
+function matchEmail(group: AbstractControl): {[key: string]: any} | null {
+  const emailControl = group.get('email');
+  const confirmEmailControl = group.get('confirmEmail');
+  if (emailControl.value === confirmEmailControl.value || confirmEmailControl.pristine) {
+    return null;
+  } else {
+    // tslint:disable-next-line: object-literal-key-quotes
+    return {'emailMismatch': true };
+  }
+
 }
